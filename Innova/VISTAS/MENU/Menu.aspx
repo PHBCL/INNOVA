a8d9e2219623df6d6029fd7d046be3a415765ca2
phbcl <pedrohurtadob@gmail.com>
2013-04-24 18:39:33 -0300

    mejora
    Signed-off-by: phbcl <pedrohurtadob@gmail.com>

--- Innova/VISTAS/MENU/Menu.aspx
+++ Innova/VISTAS/MENU/Menu.aspx
@@ -1,16 +1,22 @@
 ﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="menu.aspx.cs" Inherits="Innova.VISTAS.MENU.Menu" %>
-
 <!DOCTYPE html>
 <html lang="">
 <head>
 	<meta charset="utf-8">
 	<title></title>
-	<meta name="description" content="" />
-	<meta name="keywords" content="" />
-	<meta name="robots" content="" />
-	<link type="text/css" rel="stylesheet" href="../../css/menu/style.css" media="all" />
     <script src="../../js/actions/jsMenu.js"></script>
+    <script>
+        //Logica para cambiar tags.
+        var contenidoAnterior = "bienvenida";
+        function cambiarContenido(contenidoVer) {
+            document.getElementById(contenidoAnterior).style.display = "none";
+            contenidoAnterior = contenidoVer;
+            document.getElementById(contenidoAnterior).style.display = "block";
+        }
+
+    </script>
 </head>
+
 <body onload="ObtenerDatos();">
   <div class="sidebar">
 	<div class="search">
@@ -21,7 +27,6 @@
 	</ul>
 </div>
 
-
 <div id="bienvenida">
     <div class="content">
 	 <p>Bienvenido	 </p>
@@ -29,7 +34,19 @@
 </div> 
 <div id="misIdead" style="display:none">
     <div class="content">
-	    <p>idead orem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem. Investigationes demonstraverunt lectores legere me lius quod ii legunt saepius. Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum. Mirum est notare quam littera gothica, quam nunc putamus parum claram, anteposuerit litterarum formas humanitatis per seacula quarta decima et quinta decima. Eodem modo typi, qui nunc nobis videntur parum clari, fiant sollemnes in futurum.</p>
+	 <table >
+         <tr>
+             <td>Nombre Idea</td>
+             <td>Fecha Ingreso Idea</td>
+             <td>Acciones</td>
+         </tr>
+         <tr>
+             <td>Prueba</td>
+             <td>10/02/1987</td>
+             <td><input type="button" value="ver detalle" /><input type="button" value="eliminar" /></td>
+         </tr>
+	 </table>
+
     </div>
 </div> 
 <div id="postulaciones" style="display:none">
--- Innova/js/actions/jsMenu.js
+++ Innova/js/actions/jsMenu.js
@@ -1,43 +1,34 @@
 ﻿
 //Logica para obtencion de mensajes
-
 function ObtenerDatos()
 {
-    var cantididadMensajes=0, cantidadPostulantes=0, cantidadIdeas=0;
-
+    var cantididadMensajes = 0, cantidadPostulantes = 0, cantidadIdeas = 0;
     $.ajax({
         type: "POST",
+        async : false,
         url: "/servicios/ServicioMenu.asmx/ObtenerCantidades",
         contentType: "application/json; charset=utf-8",
         dataType: "json",
-        success: function (msg) {
-            if (msg.d != null)
-            {
+        success: function (msg)
+        {
                 cantididadMensajes =msg.d.cantidadMensajes;
                 cantidadPostulantes = msg.d.cantidadPostulantes;
                 cantidadIdeas = msg.d.cantidadIdeas;
-            }
+                var texto = '<li><a href="#" onclick="cambiarContenido('+"'"+"misIdead"+"'"+');">Mis Ideas <span class="pip">' + cantidadIdeas + '</span></a></li>';
+                texto += '<li><a href="#" onclick="cambiarContenido(' + "'" + "postulaciones" + "'" + ');">Postulaciones<span class="pip">' + cantidadPostulantes + '</span></a></li>';
+                texto += '<li>';
+                texto += '<a href="#">Mensajes <span class="pip">' + cantididadMensajes + '</span></a>';
+                texto += '<ul class=' + "'" + "submenu" + "'" + '>';
+                texto += '<li><a href="#" onclick="cambiarContenido();">Inbox</a></li>';
+                texto += '<li><a href="#" onclick="cambiarContenido();">Mensajes Eliminados</a></li>';
+                texto += '</ul>';
+                texto += '</li>';
+                texto += '<li><a href="#" onclick="cambiarContenido(' + "'" + "configuracion" + "'" +' );">Configuracion</a></li>';
+                document.getElementById('menu').innerHTML = texto;
         }
     });
-
-    var texto = '<li><a href="#" onclick="cambiarContenido("misIdead");">Mis Ideas <span class="pip">' + cantidadIdeas + '</span></a></li>';
-    texto += '    <li><a href="#" onclick="cambiarContenido("postulaciones");">Postulaciones<span class="pip">' + cantidadPostulantes + '</span></a></li>';
-    texto +=        '<li>';
-    texto +=	   '<a href="#">Mensajes <span class="pip">'+cantididadMensajes+'</span></a>';		
-    texto +=	   '<ul class="submenu">';
-    texto +=	   '<li><a href="#" onclick="cambiarContenido();">Inbox</a></li>';
-    texto +=	   '<li><a href="#" onclick="cambiarContenido();">Mensajes Eliminados</a></li>';
-    texto +=		'</ul>'
-    texto +=	'</li>'
-    texto += '<li><a href="#" onclick="cambiarContenido("configuracion");">Configuracion</a></li>';
-    document.getElementById('menu').innerHTML = texto;
+   
+    $("head").append($('<link type="text/css" rel="stylesheet" href="../../css/menu/style.css" media="all" />'));
 }
 
 
-//Logica para cambiar tags.
-var contenidoAnterior = "bienvenida";
-function cambiarContenido(contenidoVer) {
-    document.getElementById(contenidoAnterior).style.display = "none";
-    contenidoAnterior = contenidoVer;
-    document.getElementById(contenidoAnterior).style.display = "block";
-}
